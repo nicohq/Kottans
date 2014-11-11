5b952414e9682b8056f5f65cde59add194639d84
Nico Trygub <bestnico@gmail.com>
2014-11-11 23:56:29 +0200

    udp deepcopy

--- 05-JS-Basics/deepcopy.js
+++ 05-JS-Basics/deepcopy.js
@@ -1,14 +1,16 @@
+'use strict';
+
 function deepCopy(obj) {
+    //If obj = Array
+    if(obj instanceof Array) return obj.slice();
+
+    // If obj = string, number, bool, regexp, date...
+    if (Object.prototype.toString.call(obj) !== '[object Object]') return obj;
+
     var keys = Object.keys(obj),
         result = {};
-    // Error while obj == string, number, bool, regexp, array...
-    if (Object.prototype.toString.call(obj) !== '[object Object]') {
-        throw new Error('It\'s type isn\'t supported');
-    }
-
-    keys.forEach(function (key) {
+    keys.forEach(function(key) {
         result[key] = typeof obj[key] === 'object' ? deepCopy(obj[key]) : obj[key];
     });
-
     return result;
 }
